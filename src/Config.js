15904672c867ff0b2c5ae9003952129fdd3aa2b8
Surajtomar <58520961+Surajtomar@users.noreply.github.com>
2020-09-25 15:11:46 +0530

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,10 +1,3 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyB92UIfUGihAQ7sax36Ur5eYgZ2lOm0T8c",
-  authDomain: "apppost-1112.firebaseapp.com",
-  databaseURL: "https://apppost-1112.firebaseio.com",
-  projectId: "apppost-1112",
-  storageBucket: "apppost-1112.appspot.com",
-  messagingSenderId: "802252503297",
-  appId: "1:802252503297:web:f21b6c87b036a21d858300",
-  measurementId: "G-26N659JM4N",
+// Enter Your Firebase Congig
 };
